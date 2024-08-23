92a1116d0e4d340be2c5f4b59deb4632c0b83dfc
saban <sabanamdari20@gmail.com>
2024-08-23 14:58:29 -0400

    Define the handlepublishClick and creating the alert and route the alert to the Homepage

--- src/components/UploadVideo/UploadVideo.jsx
+++ src/components/UploadVideo/UploadVideo.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import upload__video from "../../Assets/Images/Upload-video-preview.jpg";
 import publish from "../../Assets/Icons/publish.svg";
 import "./UploadVideo.scss";
+import { useNavigate } from "react-router-dom";
 
 const UploadVideo = () => {
+  const navigate = useNavigate();
+
+  const handlePublishClick = () => {
+    // Display a custom alert with a button to navigate to the home page
+    const confirmAction = window.confirm(
+      'Your video is uploaded successfully. Do you want to go to the home page?'
+    );
+
+    if (confirmAction) {
+      navigate('/'); // Redirect to the home page
+    }
+  };
+
   return (
     <div className="upload">
       <h1 className="upload__title">Upload Video</h1>
@@ -19,30 +33,37 @@ const UploadVideo = () => {
         <div className="upload__video">
           <label className="upload__title-txt-label">
             TITLE YOUR VIDEO
-            <input type="text" className="upload__title-video-txt" placeholder="Add a title to your video"></input>
+            <input
+              type="text"
+              className="upload__title-video-txt"
+              placeholder="Add a title to your video"
+            />
           </label>
           <label className="upload__des-txt-label">
             ADD A VIDEO DESCRIPTION
-            <textarea className="upload__des-video-txt"placeholder="Add a description to your video" />
+            <textarea
+              className="upload__des-video-txt"
+              placeholder="Add a description to your video"
+            />
           </label>
         </div>
       </div>
       <div className="upload__buttons">
-        
-          <button type="submit" className="upload__btn-cancel">
-            CANCEL
-          </button>
-        
-      
-          <button type="submit" className="upload__btn-publish">
-            <img
-              className="upload__publish-icon"
-              src={publish}
-              alt="Publish Icon"
-            />
-            PUBLISH
-          </button>
-        
+        <button type="button" className="upload__btn-cancel">
+          CANCEL
+        </button>
+        <button
+          type="button"
+          className="upload__btn-publish"
+          onClick={handlePublishClick}
+        >
+          <img
+            className="upload__publish-icon"
+            src={publish}
+            alt="Publish Icon"
+          />
+          PUBLISH
+        </button>
       </div>
     </div>
   );
