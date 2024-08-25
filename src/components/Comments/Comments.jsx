abd54138a09e6635b690046d1d135497f23e733b
saban <sabanamdari20@gmail.com>
2024-08-25 10:36:44 -0400

    adding changes of Comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -10,16 +10,6 @@ const Comments = ({ comments }) => {
     <div  className="comments">
       {sortedComments.length > 0 ? (
         sortedComments.map((comment) => (
-          // <div key={comment.id} className='comments__container'>
-          //   <div className='comments__avatar-info'>
-          //     <div className='comments__avatar'></div>
-          //     <div className="comments__info">
-          //       <div className="comments__name"><h3>{comment.name}</h3></div>
-          //       <div className="comments__timestamp">{new Date(comment.timestamp).toLocaleString()}</div>
-          //     </div>
-          //   </div>
-          //   <div className="comments__text">{comment.comment}</div>
-          // </div>
           <Comment  key={comment.id} comment={comment}/>
         ))
       ) : (
